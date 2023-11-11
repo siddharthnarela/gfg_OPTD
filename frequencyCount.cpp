758ca5b0116046b8e2aba63989e338eb8038051b
siddharthnarela <siddharthnarela7@gmail.com>
2023-11-11 11:22:20 +0530

    Adding a new gfg_POTD

---
+++ frequencyCount.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void frequencyCount(vector<int>& arr, int N, int P) {
+        int offset = P + 1;
+
+        for (auto i : arr) {
+            int val = (i - 1) % offset;
+            if (val < N) {
+                arr[val] += offset;
+            }
+        }
+        
+        for (int i = 0; i < N; ++i) {
+            arr[i] /= offset;
+        }
+    }
+};
