ea6a3cdacaf9c3fb6565291417176edf77b7d12f
siddharthnarela <siddharthnarela7@gmail.com>
2023-11-12 12:49:41 +0530

    Function to check if a string can be obtained by rotating

---
+++ isRotated.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isRotated(string s1, string s2) {
+        if (s1 == s2.substr(2) + s2.substr(0, 2))
+            return true;
+        if (s1 == s2.substr(s2.size() - 2) + s2.substr(0, s2.size() - 2))
+            return true;
+
+        return false;
+    }
+};
